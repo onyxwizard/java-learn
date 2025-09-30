670f4c8c01d1a5a78d0bd5438d64701fdc2d0a1b
AK <67966017+onyxwizard@users.noreply.github.com>
2025-09-30 14:06:08 +0530

    feat (oops): Added basic Java object oriented programming concept

---
+++ src/phase1/java/oop/fleet/model/Drivable.java
@@ -0,0 +1,8 @@
+// phase1.java.chapter1/fleet/model/Drivable.java
+package phase1.java.oop.fleet.model;
+
+interface Drivable {
+    void start();
+    void stop();
+    void accelerate(int increment);
+}
